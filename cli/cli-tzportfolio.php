3b6926f797893a66215c2c69e185f7f678beba00
root <root@ip-10-212-89-28.ec2.internal>
2013-01-29 03:50:18 +0000

    command line detects ratings, problem with tiff files

--- cli/cli-tzportfolio.php
+++ cli/cli-tzportfolio.php
@@ -16,7 +16,6 @@ require_once( JPATH_BASE .DS.'..'.DS.'libraries'.DS.'joomla'.DS.'user'.DS.'user.
 require_once( JPATH_BASE .DS.'..'.DS.'libraries'.DS.'joomla'.DS.'application'.DS.'component'.DS.'helper.php' );
 // Define component path.
 
-
 require_once( JPATH_BASE .DS.'components'.DS.'com_tz_portfolio'.DS.'models'.DS.'article.php' );
 
 jimport('joomla.database.database');
@@ -34,7 +33,7 @@ $options        = array ( 'driver' => $jconfig->dbtype,
 
 $mainframe      =& JFactory::getApplication('administrator');
 
-$file = file_get_contents('/var/www/html/paper/appdata.txt');
+$file = file_get_contents('/var/www/html/appdata.txt');
 $appdata = json_decode($file);
 
 if(!isset($appdata))
@@ -55,23 +54,73 @@ foreach($appdata as $id => $app)
 	foreach($app as $k => $ap)
 	{
 
-	//echo $k .' = '.print_r($ap);exit(0);
-	$tz = new TZ_PortfolioModelArticle();
-
-	$data = array();
-
-	$data['title'] = $ap->title; //Fusce convallis accumsan
-	$data['alias'] = str_replace(" ","_",$ap->title);
-	$data['articletext'] = $ap->description; 
-
 	
-	if(isset($ap->image_url))
-		$data['tz_img_gallery_server'][] = downloadFile($ap->image_url,'/tmp/file_cache_'.array_sum( explode( ' ' , microtime() ) ).'.jpg'); //$ap->image_url; 
-	elseif (isset($ap->thumbnail_url_big))
-		$data['tz_img_gallery_server'][] = downloadFile($ap->thumbnail_url_big,'/tmp/file_cache_'.array_sum( explode( ' ' , microtime() ) ).'.jpg'); //$ap->image_url; 
 
-	if(isset($data['tz_img_gallery_server'][0]))
-	{
+		$primaryDb =& JDatabase::getInstance( $options );
+		$q = 'SELECT contentid FROM #__tz_portfolio WHERE value=\''.$ap->mimvi_app_id.'\'';
+		$primaryDb->setQuery($q);
+		$result = $primaryDb->loadResult();
+
+		if(isset($result) && $result != '')
+		{
+			echo 'app is already in the db...'.$ap->mimvi_app_id.PHP_EOL;
+		} else {
+
+			$content = '<p>By: '.$ap->author.'</p>';
+			$content .= '<p>Available: <a href="'.$ap->download_url.'">'. ucfirst($ap->platform) .' Store</a></p>';
+			$content .= '<p>'.$ap->description.'</p><p></p>';
+			//echo $k .' = '.print_r($ap);exit(0);
+			$tz = new TZ_PortfolioModelArticle();
+
+			$data = array();
+
+			$data['title'] = $ap->title; 
+			$data['alias'] = str_replace(" ","_",$ap->title);
+
+			if($ap->rating == '1.0' || $ap->rating == '1.5')
+			$rating = 0;
+			switch($ap->rating)
+			{
+				case '1.0':
+				case '0.5';
+				case '0.0';
+					$rating = 1;
+					break;
+				case '2.0':
+				case '2.5';
+					$rating = 2;
+					break;
+				case '3.0':
+				case '3.5';
+					$rating = 3;
+					break;
+				case '4.0':
+				case '4.5';
+					$rating = 4;
+					break;
+				case '5.0':
+				case '5.5';
+					$rating = 5;
+					break;
+				default:
+					$rating = 0;
+			}
+
+			if($rating != 0)
+				$content .= '<p>App Store Rating<br /><img src="images/'.$rating.'-stars.png" border="0" height="64" style="line-height: 1.3em; border: 0;" /></p>';
+	
+			if(isset($ap->thumbnail_url_big) && 
+				(strpos($ap->thumbnail_url_big,'tif') != strlen($ap->thumbnail_url_big)-3) &&
+				(strpos($ap->thumbnail_url_big,'tif') != strlen($ap->thumbnail_url_big)-4))
+			{
+				$data['tz_img_gallery_server'][] = downloadFile($ap->thumbnail_url_big,'/tmp/file_cache_'.array_sum( explode( ' ' , microtime() ) ).'.jpg'); //$ap->image_url; 
+			
+			}
+			elseif (isset($ap->image_url))
+				$data['tz_img_gallery_server'][] = downloadFile($ap->image_url,'/tmp/file_cache_'.array_sum( explode( ' ' , microtime() ) ).'.jpg'); //$ap->image_url; 
+
+			if(isset($data['tz_img_gallery_server'][0]))
+			{
 
 	/*$data['tz_attachments_file'] = array(	'name' => 	
 						array($data['tz_img_gallery_server'][0]),
@@ -79,37 +128,77 @@ foreach($appdata as $id => $app)
 						'tmp_name' => array($data['tz_img_gallery_server'][0]),
 						'size' =>  array(filesize($data['tz_img_gallery_server'][0])));
 	*/
-	$data['tz_img'] = $data['tz_img_gallery_server'][0];
-		
-	$data['state'] = 1;
-	$data['catid'] = 20;
-	$data['created'] = date('Y-m-d'); // 2012-06-25 09:22:10
-	$data['created_by'] = 203;
-	$data['access'] = 1;
-	$data['featured'] = 0;
-	$data['attribs'] = array();
-	$data['attribs']['adjustX'] = 0;
-	$data['attribs']['adjustY'] = 0;
-	$data['attribs']['tint'] = 0;
-	$data['attribs']['tintOpacity'] = 0.5;
-	$data['attribs']['lensOpacity'] = 0.5;
-	$data['attribs']['softFocus'] = 0;
-	$data['attribs']['smoothMove'] = 3;
-	$data['attribs']['titleOpacity'] = 0.5;
-	$data['attribs']['image_gallery_amination'] = 'none';
-	$data['attribs']['image_gallery_animSpeed'] = 7000;
-	$data['attribs']['image_gallery_animation_duration'] = 600;
-	$data['groupid'] = 1;
-	$data['type_of_media'] = 'image';
+				$data['tz_img'] = $data['tz_img_gallery_server'][0];
+				$data['articletext'] = $content;		
+				$data['tzfields2'] = '&lt;a href=&quot;'.$ap->download_short_url.'&quot; target=&quot;_self&quot;&gt;Download&lt;/a&gt;';
+				$data['tzfields3'] = $ap->platform; 
+				$data['tzfields4'][] = $ap->rating; 
+				$data['tzfields5'] = $ap->num_ratings; 
+				$data['tzfields6'] = $ap->author; 
+				$data['tzfields7'] = $ap->price; 
+				$data['tzfields8'] = $ap->mimvi_app_id; 
+				$data['tzfields9'] = $ap->id; 
+				$data['state'] = 1;
 
-	$result = $tz->saveCli($data);
+				switch ($ap->platform) {
+					case 'itunes':
+						$data['catid'] = 8;
+						break;
+					case 'android':
+						$data['catid'] = 22;
+						break;
+					case 'blackberry':
+						$url = substr($ap->image_url,0,strlen($ap->image_url)-1).'12';
+						$data['tz_img_gallery_server'][0] = downloadFile($url, '/tmp/file_cache_'.array_sum( explode(' ', microtime() )).'.png');
+						$data['catid'] = 13;
+						break;
+					case 'windows':
+						$data['catid'] = 12;
+						break;
+					case 'ipad':
+						$data['catid'] = 9;
+						break;
+					case 'iphone':
+						$data['catid'] = 10;
+						break;
+					case 'facebook':
+						$data['catid'] = 20;
+						break;
+					case 'mobileweb':
+						$data['catid'] = 14;
+						break;
+					default:
+						$data['catid'] = 8;
+				}
 
-	if($result == true)
-		echo 'saved article...'.PHP_EOL;
-	else
-		echo 'did not save '.$data['tz_img'].PHP_EOL;
+				$data['created'] = date('Y-m-d'); // 2012-06-25 09:22:10
+				$data['created_by'] = 203;
+				$data['access'] = 1;
+				$data['featured'] = 0;
+				$data['attribs'] = array();
+				$data['attribs']['adjustX'] = 0;
+				$data['attribs']['adjustY'] = 0;
+				$data['attribs']['tint'] = 0;
+				$data['attribs']['tintOpacity'] = 0.5;
+				$data['attribs']['lensOpacity'] = 0.5;
+				$data['attribs']['softFocus'] = 0;
+				$data['attribs']['smoothMove'] = 3;
+				$data['attribs']['titleOpacity'] = 0.5;
+				$data['attribs']['image_gallery_amination'] = 'none';
+				$data['attribs']['image_gallery_animSpeed'] = 7000;
+				$data['attribs']['image_gallery_animation_duration'] = 600;
+				$data['groupid'] = 2;
+				$data['type_of_media'] = 'image';
 
-	}
+				$result = $tz->saveCli($data);
+
+				if($result == true)
+					echo 'saved article...'.PHP_EOL;
+				else
+					echo 'did not save '.$data['tz_img'].PHP_EOL;
+
+			}
+		}
 	}
 }
 
